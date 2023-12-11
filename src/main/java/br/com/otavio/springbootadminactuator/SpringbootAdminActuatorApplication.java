f1a5e252dc464c8c02905544aafefe6da8ef223f
Otavio Marques <marquesotavio251204@gmail.com>
2023-12-10 21:20:37 -0300

    create entitiy person

---
+++ src/main/java/br/com/otavio/springbootadminactuator/SpringbootAdminActuatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.otavio.springbootadminactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdminActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAdminActuatorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/otavio/springbootadminactuator/model/entity/Person.java
@@ -0,0 +1,27 @@
+package br.com.otavio.springbootadminactuator.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity(name = "person")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
