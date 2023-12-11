8e8e6aa12c4dbde8b8425568f16efe6ab12b3ed5
Otavio Marques <marquesotavio251204@gmail.com>
2023-12-10 22:14:23 -0300

    create PersonService

---
+++ src/main/java/br/com/otavio/springbootadminactuator/PersonService.java
@@ -0,0 +1,47 @@
+package br.com.otavio.springbootadminactuator;
+
+import br.com.otavio.springbootadminactuator.model.dto.PersonDto;
+import br.com.otavio.springbootadminactuator.model.entity.PersonEntity;
+import br.com.otavio.springbootadminactuator.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public void create (PersonEntity personEntity) {
+        personRepository.save(personEntity);
+    }
+
+    public List<PersonEntity> findAll () {
+        return personRepository.findAll();
+    }
+
+    public PersonEntity findById (UUID id) {
+        Optional<PersonEntity> optionalPersonEntity = personRepository.findById(id);
+
+        return optionalPersonEntity.get();
+    }
+
+    public PersonEntity update (PersonEntity personEntity) throws Exception {
+        if (!personRepository.existsById(personEntity.getId())) {
+            throw new Exception("id not found");
+        }
+
+        return personRepository.save(personEntity);
+    }
+
+    public void delete (UUID id) {
+        personRepository.delete(findById(id));
+    }
+}
