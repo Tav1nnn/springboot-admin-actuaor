efbe6ebb8fa5f152e388419722810b9ce793019a
Otavio Marques <marquesotavio251204@gmail.com>
2023-12-12 23:01:59 -0300

    config spring boot admin

--- admin/src/main/java/br/com/otavio/admin/AdminApplication.java
+++ admin/src/main/java/br/com/otavio/admin/AdminApplication.java
@@ -1,9 +1,15 @@
 package br.com.otavio.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 public class AdminApplication {
 
 	public static void main(String[] args) {
